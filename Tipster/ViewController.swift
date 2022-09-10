7f899e758e7ca769d323f257cb736ed3484cfcb1
Zhiwei Ma <Zhiweimaw@gmail.com>
2022-09-09 18:35:19 -0700

    Initial Commit

---
+++ Tipster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tipster
+//
+//  Created by Zhiwei Ma on 9/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
