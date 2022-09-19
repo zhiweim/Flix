95362d80139da047b8f1f9eda3a232910a1c4700
Zhi Ma <mazh@oregonstate.edu>
2022-09-18 18:12:58 -0700

    Added Movie Grid

---
+++ Tipster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Tipster
+//
+//  Created by Zhiwei Ma on 9/18/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+
+    @IBOutlet weak var posterView: UIImageView!
+
+}
